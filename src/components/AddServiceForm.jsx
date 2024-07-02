80d5a9e96b25a7de29cfc2fd7166ddcb09140f3e
yacermsaad <yacer.msaad@gmail.com>
2024-07-02 23:11:03 +0100

    commit-myCommit22  yasser

--- src/components/AddServiceForm.jsx
+++ src/components/AddServiceForm.jsx
@@ -2,20 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const AddServiceForm = () => {
-<<<<<<< HEAD
-  const [serviceName, setServiceName] = useState("");
-  const [serviceDescription, setServiceDescription] = useState("");
-  const [servicePrice, setServicePrice] = useState("");
-  const [images, setImages] = useState([]);
-  const [coachId, setCoachId] = useState("");
-=======
- const [serviceName,setServiceName] = useState("");
-  
+  const [serviceName, setServiceName] = useState('');
   const [serviceDescription, setServiceDescription] = useState('');
   const [servicePrice, setServicePrice] = useState('');
-  const [images, setImages] = useState('');
+  const [images, setImages] = useState([null, null, null, null, null]);
+  const [imagePreviews, setImagePreviews] = useState([null, null, null, null, null]);
   const [coachId, setCoachId] = useState('');
->>>>>>> a32ed6973e03d4020788f3e79968eaa5b332f0fb
+  const [showErrorModal, setShowErrorModal] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     const storedCoach = JSON.parse(localStorage.getItem('user'));
@@ -24,26 +18,28 @@ const AddServiceForm = () => {
     } else {
       alert('No coach found in local storage');
     }
-
   }, []);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-<<<<<<< HEAD
-    console.log(serviceName);
-=======
->>>>>>> a32ed6973e03d4020788f3e79968eaa5b332f0fb
+    if (images.filter(image => image !== null).length < 5) {
+      setErrorMessage('Please upload at least 5 images.');
+      setShowErrorModal(true);
+      return;
+    }
+
     const formData = new FormData();
     formData.append('titre', serviceName);
     formData.append('description', serviceDescription);
     formData.append('prix', servicePrice);
     formData.append('coach_id', coachId);
-<<<<<<< HEAD
-    // formData.append('images', images);
 
-=======
-    formData.append('images', images);
->>>>>>> a32ed6973e03d4020788f3e79968eaa5b332f0fb
+    images.forEach((image, index) => {
+      if (image) {
+        formData.append(`images[${index}]`, image);
+      }
+    });
+
     try {
       const response = await axios.post('http://127.0.0.1:8000/api/services', formData, {
         headers: {
@@ -59,79 +55,97 @@ const AddServiceForm = () => {
       }
       alert('Failed to create service. Please check your input and try again.');
     }
-    
-
   };
 
-  const handleImageChange = (e) => {
-    setImages(e.target.files[0]);
-<<<<<<< HEAD
-    
-=======
->>>>>>> a32ed6973e03d4020788f3e79968eaa5b332f0fb
+  const handleImageChange = (index, e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const newImages = [...images];
+      newImages[index] = file;
+      setImages(newImages);
+
+      const newImagePreviews = [...imagePreviews];
+      newImagePreviews[index] = URL.createObjectURL(file);
+      setImagePreviews(newImagePreviews);
+
+      // Clear error message if any image is added
+      if (errorMessage) {
+        setErrorMessage('');
+      }
+    }
   };
-  console.log(serviceName);
-    console.log(serviceDescription);
-    console.log(servicePrice);
-    console.log(coachId);
 
   return (
-    <form onSubmit={handleFormSubmit} className="space-y-6 p-6 bg-white rounded-lg shadow-2xl">
-      <h1 className="text-xl font-bold mb-4" style={{ textAlign: 'center' }}>Start Add Service</h1>
-      <div>
-        <label htmlFor="serviceName" className="block text-lg font-medium text-gray-700">Service Name:</label>
-        <input
-          type="text"
-          id="serviceName"
-<<<<<<< HEAD
-         
-          onChange={(e) => setServiceName(e.target.value)}
-=======
-          onChange={(e)=>{setServiceName(e.target.value)}}
->>>>>>> a32ed6973e03d4020788f3e79968eaa5b332f0fb
-          required
-          className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-md placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-      </div>
-      <div>
-        <label htmlFor="serviceDescription" className="block text-lg font-medium text-gray-700">Service Description:</label>
-        <textarea
-          id="serviceDescription"
-
-          onChange={(e) =>{ setServiceDescription(e.target.value); console.log(e.target.value)}}
-          required
-          className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-md placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          rows="4"
-        />
-      </div>
-      <div>
-        <label htmlFor="servicePrice" className="block text-lg font-medium text-gray-700">Service Price:</label>
-        <input
-          type="number"
-          id="servicePrice"
-          
-          onChange={(e) => setServicePrice(e.target.value)}
-          required
-          className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-md placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-      </div>
-      <div>
-        <label htmlFor="images" className="block text-lg font-medium text-gray-700">Upload Images:</label>
-        <input
-          type="file"
-          id="images"
-          multiple
-          onChange={handleImageChange}
-          className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-md placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-      </div>
-      <button
-        type="submit"
-        className="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-lg text-sm font-medium rounded-md text-white bg-green-500 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-      >
-        Add Service
-      </button>
-    </form>
+    <div>
+      {showErrorModal && (
+        <div className="fixed inset-0 bg-gray-600 bg-opacity-75 flex items-center justify-center z-50">
+          <div className="bg-white rounded-lg p-6 shadow-lg w-96">
+            <h2 className="text-xl font-bold mb-4">Error</h2>
+            <p>{errorMessage}</p>
+            <button
+              onClick={() => setShowErrorModal(false)}
+              className="mt-4 inline-flex justify-center py-2 px-4 border border-transparent shadow-lg text-sm font-medium rounded-md text-white bg-red-500 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+      <form onSubmit={handleFormSubmit} className="space-y-6 p-6 bg-white rounded-lg shadow-2xl">
+        <h1 className="text-xl font-bold mb-4" style={{ textAlign: 'center' }}>Start Add Service</h1>
+        <div>
+          <label htmlFor="serviceName" className="block text-lg font-medium text-gray-700">Service Name:</label>
+          <input
+            type="text"
+            id="serviceName"
+            onChange={(e) => setServiceName(e.target.value)}
+            required
+            className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-md placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+        <div>
+          <label htmlFor="serviceDescription" className="block text-lg font-medium text-gray-700">Service Description:</label>
+          <textarea
+            id="serviceDescription"
+            onChange={(e) => setServiceDescription(e.target.value)}
+            required
+            className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-md placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            rows="4"
+          />
+        </div>
+        <div>
+          <label htmlFor="servicePrice" className="block text-lg font-medium text-gray-700">Service Price:</label>
+          <input
+            type="number"
+            id="servicePrice"
+            onChange={(e) => setServicePrice(e.target.value)}
+            required
+            className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-md placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+        <div>
+          <label className="block text-lg font-medium text-gray-700">Upload Images:</label>
+          {images.map((image, index) => (
+            <div key={index} className="mt-2">
+              <input
+                type="file"
+                onChange={(e) => handleImageChange(index, e)}
+                className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-md placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+              {imagePreviews[index] && (
+                <img src={imagePreviews[index]} alt={`Preview ${index}`} className="w-24 h-24 object-cover rounded-md shadow-md mt-2" />
+              )}
+            </div>
+          ))}
+        </div>
+        <button
+          type="submit"
+          className="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-lg text-sm font-medium rounded-md text-white bg-green-500 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Add Service
+        </button>
+      </form>
+    </div>
   );
 };
 
--- src/components/pro.jsx
+++ src/components/pro.jsx
@@ -36,6 +36,7 @@ export default function ProfilePage() {
     const [showFormSkill, setShowFormSkill] = useState(false);
   
     const [postData, setPostData] = useState(null);
+    
     const [description, setDescription] = useState(''); 
   const [isEditing, setIsEditing] = useState(false);
     const [isOpen, setIsOpen] = useState(false);
@@ -574,65 +575,8 @@ export default function ProfilePage() {
       <div className="col-md-6">
         <div className="card" style={{ marginLeft: "0px", width: "860px", background: "white",  padding: "20px", borderRadius: "10px" }}>
           <div>
-<<<<<<< HEAD
-          {blogFrom ? (
-            <form onSubmit={handleFormSubmitService} className="space-y-6 p-6 bg-white rounded-lg shadow-xl">
-    <h1 className="text-xl font-bold mb-4" style={{ textAlign: 'center' }}>Start Add Service</h1>
-    <div>
-      <label htmlFor="serviceName" className="block text-lg font-medium text-gray-700">Service Name:</label>
-      <input
-        type="text"
-        id="serviceName"
-        value={serviceName}
-        onChange={(e) => setServiceName(e.target.value)}
-        required
-        className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-md placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-      />
-    </div>
-    <div>
-      <label htmlFor="serviceDescription" className="block text-lg font-medium text-gray-700">Service Description:</label>
-      <textarea
-        id="serviceDescription"
-        value={serviceDescription}
-        onChange={(e) => setServiceDescription(e.target.value)}
-        required
-        className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-md placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        rows="4"
-      />
-    </div>
-    <div>
-      <label htmlFor="servicePrice" className="block text-lg font-medium text-gray-700">Service Price:</label>
-      <input
-        type="number"
-        id="servicePrice"
-        value={servicePrice}
-        onChange={(e) => setServicePrice(e.target.value)}
-        required
-        className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-md placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-      />
-    </div>
-   
-    <div>
-      <label htmlFor="images" className="block text-lg font-medium text-gray-700">Upload Images:</label>
-      <input
-        type="file"
-        id="images"
-        multiple
-        onChange={handleImageChange}
-        className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-md placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-      />
-    </div>
-    <button
-      type="submit"
-      className="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-lg text-sm font-medium rounded-md text-white bg-green-500 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-    >
-      Add Service
-    </button>
-  </form>
-=======
             {blogFrom ? (
              <AddServiceForm />
->>>>>>> a32ed6973e03d4020788f3e79968eaa5b332f0fb
 
           ) : null}
           </div>
